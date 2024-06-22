8d9417a8a294cd9615e981488ccd2994646a2275
Zach Fischer <zfischer42@gmail.com>
2024-06-22 15:14:24 -0700

    Basic navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageArchiveComponent } from './page-archive/page-archive.component';
+import { PageArticleComponent } from './page-article/page-article.component';
+import { PageHomeComponent } from './page-home/page-home.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    RouterModule, 
+    PageAboutComponent,
+    PageArchiveComponent, 
+    PageArticleComponent, 
+    PageHomeComponent,
+    NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
