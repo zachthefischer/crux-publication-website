41631902b0d5bbe66ec68c0c6bddb55b2a376d12
Zach Fischer <zfischer42@gmail.com>
2024-07-15 21:54:20 -0700

    Initial commit. Svelte-kit, tailwind, supabase packages installed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light"]
+  },
+};
