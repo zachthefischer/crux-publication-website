bf55defe520905ba2566a32a81fd8441a153f3e5
Zach Fischer <zfischer42@gmail.com>
2024-08-11 17:54:59 -0700

    Backdoor article creation frontend

--- src/routes/technology/+page.svelte
+++ src/routes/technology/+page.svelte
@@ -76,7 +76,10 @@
 <!-- Search/Sort Articles -->
 <div class="mx-auto w-3/5 mg:w-1/2 lg:w-2/5 my-0 pb-0 md:py-3 md:my-3 z-10">
     <div class="flex flex-row justify-center">
-        <input class="input input-bordered w-80" placeholder="Search Articles"/>
+        <label class="input input-bordered flex items-center gap-2 w-80 md:w-96 ms-2">
+            <input type="text" class="grow" placeholder="Search Articles" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70"> <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /> </svg>
+        </label>
 
         <select 
             bind:value={selectedSort} 
