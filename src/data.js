9cc9cea4ff3e061181565dee350b7674b98f53f7
Zach Fischer <zfischer42@gmail.com>
2024-09-30 15:04:33 -0700

    Updated dummy articles to be more palettable

--- src/data.js
+++ src/data.js
@@ -1,82 +1,72 @@
 export const articleData = [
     {
         slug: "one", 
-        title: "Living La Vida Loca", 
+        title: "Article One", 
         category: "Business",
-        description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds. The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
         img:"https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp", 
-        author: "Jane Goodwont", 
+        author: "Jane Doe", 
         date: "Jan 1, 2022", 
-        content: "In recent years, electric vehicles (EVs) have surged in popularity, revolutionizing the automotive industry. The push towards sustainable transportation is driven by growing environmental concerns and advancements in battery technology. EVs offer numerous benefits, including reduced greenhouse gas emissions, lower running costs, and less dependency on fossil fuels. Companies like Tesla, Nissan, and BMW are at the forefront of this movement, continuously innovating to increase the range and efficiency of their models. As charging infrastructure expands and battery costs decrease, the future of transportation looks increasingly electric."
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         slug: "two", 
-        title: "Why Brains Smell Like Cheese", 
+        title: "Article Two", 
         category: "Business",
-        description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds. The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
         img:"https://neurosciencenews.com/files/2023/12/log-covid-brain-changes-neurosicence.jpg", 
-        author: "Jane Goodwont", 
+        author: "Jane Doe", 
         date: "Jan 1, 2022",
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         slug: "three", 
-        title: "The Physical Impossibility of Death", 
+        title: "Article Three", 
         category: "Technology",
-        description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds. The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
         img:"https://images.theconversation.com/files/423869/original/file-20210929-2257-118emfh.jpg?ixlib=rb-4.1.0&rect=19%2C39%2C3280%2C2430&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip", 
-        author: "Jane Goodwont", 
+        author: "Jane Doe", 
         date: "Jan 1, 2022",
-        content: "Mindfulness meditation has gained significant traction as a powerful tool for enhancing mental well-being. This practice involves focusing on the present moment, acknowledging and accepting one’s thoughts and feelings without judgment. Research has shown that regular mindfulness meditation can reduce stress, anxiety, and depression, while improving attention and emotional regulation. Incorporating just a few minutes of mindfulness into your daily routine can lead to a calmer, more centered life. Whether through guided sessions or self-practice, mindfulness offers a path to inner peace and resilience."
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         slug: "four", 
-        title: "The Legacy of Joever Bidone ", 
+        title: "Article Four ", 
         category: "Science",
-        description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds. The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
-        img:"https://a.pinatafarm.com/1420x946/de000dbcb1/big-brain-0ef8b26fa0a27e58a297491dc8d17f83-meme.jpeg", 
-        author: "Jane Goodwont", 
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
+        img:"https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F856ba79a-ad8e-4626-9a68-b39922e41960_1181x1299.jpeg", 
+        author: "Jane Doe", 
         date: "Jan 1, 2022",
-        content: "Artificial Intelligence (AI) has evolved dramatically over the past few decades, transforming from a niche field into a cornerstone of modern technology. AI encompasses a range of technologies, including machine learning, neural networks, and natural language processing. Applications of AI are vast, from virtual assistants like Siri and Alexa to complex data analysis in healthcare and finance. AI systems can learn and adapt, making them invaluable for tasks that require pattern recognition and decision-making. As AI continues to advance, ethical considerations regarding privacy, bias, and job displacement remain critical discussions in the tech community."
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         slug: "five", 
-        title: "Image Not Found: A Memoir", 
+        title: "Article Five", 
         category: "Business",
-        description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds. The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
         img:"https://www.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600nw-1037719192.jpg", 
-        author: "Jane Goodwont", 
+        author: "Jane Doe", 
         date: "Jan 1, 2022",
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    // {
-    //     slug: "six", 
-    //     title: "Image Not Found: A Memoir The Sequel", 
-    //     category: "Business",
-    //     description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
-    //     img:"https://www.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600nw-1037719192.jpg", 
-    //     author: "Jane Goodwont", 
-    //     date: "Jan 1, 2022",
-    //     content: "james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper "
-    // },
-    // {
-    //     slug: "seven", 
-    //     title: "More examples!", 
-    //     category: "Business",
-    //     description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
-    //     img:"https://www.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600nw-1037719192.jpg", 
-    //     author: "Jane Goodwont", 
-    //     date: "Jan 1, 2022",
-    //     content: "james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper "
-    // },
-    // {
-    //     slug: "eight", 
-    //     title: "Even more examples!!", 
-    //     category: "Science",
-    //     description: "The story of a young boy who lived in the forrest with rats and raccoons and a handful of birds.", 
-    //     img:"https://www.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600nw-1037719192.jpg", 
-    //     author: "Jane Goodwont", 
-    //     date: "Jan 1, 2022",
-    //     content: "james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper james green is an absolute yapper "
-    // },
+    {
+        slug: "six", 
+        title: "Article Six", 
+        category: "Business",
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
+        img:"https://content.health.harvard.edu/wp-content/uploads/2024/01/b2886d73-54ce-4591-a206-3c5287083084.jpg", 
+        author: "Jane Doe", 
+        date: "Jan 1, 2022",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        slug: "seven", 
+        title: "Article Seven", 
+        category: "Technology",
+        description: "The CruX Publication team is hard at work creating articles. Please come back soon for interesting stories and the latest in neuroscience news.", 
+        img:"https://media.istockphoto.com/id/1168179082/photo/man-with-brain-stroke-symptoms.jpg?s=612x612&w=0&k=20&c=eYZ9ayO0rR0Su3vN3EU48CxOOnwxUXDgXhpAYb2s7L4=", 
+        author: "Jane Doe", 
+        date: "Jan 1, 2022",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
 ]
