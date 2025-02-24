af84f8be90c32ae6af4d36ce134c2546843bb0f3
Zach Fischer <zfischer42@gmail.com>
2025-02-23 20:02:54 -0800

    Add Varun profile

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,6 +7,7 @@
 	import pfp_jonny from '$lib/assets/pfps/Jonny_Dippery_Writer.jpg';
 	import pfp_ting from '$lib/assets/pfps/Ting_Wu_Analyst.png';
 	import pfp_abby from '$lib/assets/pfps/Abby_Lin_Author.jpeg';
+	import pfp_arun from '$lib/assets/pfps/Varun_Sekar_TechAnalyst.jpeg';
 
 	const team = [
 		{
@@ -44,6 +45,12 @@
 			profilePic: pfp_abby,
 			linkedin: 'https://www.instagram.com/abs1626productions/',
 			title: 'Author'
+		},
+		{
+			name: 'Arun Sekar',
+			profilePic: pfp_arun,
+			linkedin: 'https://www.linkedin.com/in/varun-sekar-2b2247339/',
+			title: 'Tech Analyst'
 		}
 	];
 </script>
